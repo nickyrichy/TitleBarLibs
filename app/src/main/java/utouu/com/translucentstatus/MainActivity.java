d2e433b99048858c5ed6c368fee6de1f78c7add4
李良海 <liang-hai.li@utsoft.cn>
2016-10-11 17:19:01 +0800

    TitleBarLibs version 1

---
+++ app/src/main/java/utouu/com/translucentstatus/MainActivity.java
@@ -0,0 +1,19 @@
+package utouu.com.translucentstatus;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 李良海 on 2016/10/11 15:17.
+ * Function:
+ * Desc:
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
